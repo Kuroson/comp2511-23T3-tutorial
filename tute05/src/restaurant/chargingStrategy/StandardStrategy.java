2080281245a9912b7b9206c6987fd3b62362df7b
Alvin Cherk <a.cherk@unsw.edu.au>
2023-10-09 14:58:50 +1100

    Add tute05

--- tute05/src/restaurant/chargingStrategy/StandardStrategy.java
+++ tute05/src/restaurant/chargingStrategy/StandardStrategy.java
@@ -1,8 +1,26 @@
 package restaurant.chargingStrategy;
 
+import java.util.List;
+
+import restaurant.Meal;
+
 /**
  * Standard - normal rates
  */
-public class StandardStrategy {
+public class StandardStrategy implements ChargingStrategy {
+    private static final double MODIFIER = 1;
 
+    /**
+     * The cost of a meal.
+     */
+    public double cost(List<Meal> order, boolean payeeIsMember) {
+        return order.stream().mapToDouble(meal -> meal.getCost()).sum();
+    }
+
+    /**
+     * Modifying factor of charges for standard customers.
+     */
+    public double standardChargeModifier() {
+        return MODIFIER;
+    }
 }
