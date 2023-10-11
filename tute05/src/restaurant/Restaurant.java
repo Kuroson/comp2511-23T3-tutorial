c5bd2cba8f7f08dcf8f64ad5fcb5f59adb6a4aa3
Alvin Cherk <a.cherk@unsw.edu.au>
2023-10-11 15:10:48 +1100

    Add week 5 code

--- tute05/src/restaurant/Restaurant.java
+++ tute05/src/restaurant/Restaurant.java
@@ -6,8 +6,12 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import restaurant.chargingStrategy.ChargingStrategy;
+import restaurant.chargingStrategy.DiscountStrategy;
+import restaurant.chargingStrategy.StandardStrategy;
+
 public class Restaurant {
-    private String chargingStrategy = "standard";
+    private ChargingStrategy chargingStrategy = new StandardStrategy();
     private String name;
     private List<Meal> menu = new ArrayList<Meal>();
     private List<String> members = new ArrayList<String>();
@@ -22,46 +26,17 @@ public class Restaurant {
         }
     }
 
+    public void setChargingStrategy(ChargingStrategy s) {
+        this.chargingStrategy = s;
+    }
+
     public double cost(List<Meal> order, String payee) {
-        switch (chargingStrategy) {
-        case "standard":
-            return order.stream().mapToDouble(meal -> meal.getCost()).sum();
-        case "holiday":
-            return order.stream().mapToDouble(meal -> meal.getCost() * 1.15).sum();
-        case "happyHour":
-            if (members.contains(payee)) {
-                return order.stream().mapToDouble(meal -> meal.getCost() * 0.6).sum();
-            } else {
-                return order.stream().mapToDouble(meal -> meal.getCost() * 0.7).sum();
-            }
-        case "discount":
-            if (members.contains(payee)) {
-                return order.stream().mapToDouble(meal -> meal.getCost() * 0.85).sum();
-            } else {
-                return order.stream().mapToDouble(meal -> meal.getCost()).sum();
-            }
-        default:
-            return 0;
-        }
+        // utilising dynamic polymorphism
+        return this.chargingStrategy.cost(order, members.contains(payee));
     }
 
     public void displayMenu() {
-        double modifier = 0;
-        switch (chargingStrategy) {
-        case "standard":
-            modifier = 1;
-            break;
-        case "holiday":
-            modifier = 1.15;
-            break;
-        case "happyHour":
-            modifier = 0.7;
-            break;
-        case "discount":
-            modifier = 1;
-            break;
-        }
-
+        double modifier = this.chargingStrategy.standardChargeModifier();
         for (Meal meal : menu) {
             System.out.println(meal.getName() + " - " + meal.getCost() * modifier);
         }
@@ -69,7 +44,14 @@ public class Restaurant {
 
     public static void main(String[] args) {
         Restaurant r = new Restaurant("XS");
-        r.displayMenu();
+        r.displayMenu(); // standard
+        r.setChargingStrategy(new DiscountStrategy());
+        r.displayMenu(); // discount;
+
+        String input = ""; // read from terminal
+        // if (input.equals("standard")) {
+        //     r.setChargingStrategy(new StandardStrategy());
+        // }
     }
 
 }
