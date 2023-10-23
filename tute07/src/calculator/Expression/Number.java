6fcce6feb59d031573b1b77efd7bb91c9c732b05
Alvin Cherk <a.cherk@unsw.edu.au>
2023-10-23 11:56:55 +1100

    Week 7 Code

--- tute07/src/calculator/Expression/Number.java
+++ tute07/src/calculator/Expression/Number.java
@@ -4,12 +4,17 @@ package calculator.Expression;
  * Leaf node
  */
 public class Number implements Expression {
+    private double num;
+
+    public Number(double num) {
+        this.num = num;
+    }
+
     /**
      * @preconditions - none
      * @postconditions - returns number
      */
     public double compute() {
-        // TODO
-        return 0;
+        return this.num;
     }
 }
--- tute07/src/thrones/Game.java
+++ tute07/src/thrones/Game.java
@@ -1,9 +1,23 @@
 package thrones;
 
 public class Game {
-    
+
+    public Character createCharacter(String type, int x, int y) {
+        if (type.equals("King")) {
+            return new King(x, y, 0);
+        } else if (type.equals("Queen")) {
+            return new Queen(x, y);
+        }
+        return null;
+    }
+
+    public King createKing(int x, int y) {
+        return new King(x, y, 0);
+    }
+
     public static void main(String[] args) {
-        King king = new King(0, 0);
+        Game g = new Game();
+        King king = g.createKing(0, 0);
         Queen queen = new Queen(0, 1);
         Knight knight = new Knight(0, 2);
         Dragon dragon = new Dragon(-1, 0);
