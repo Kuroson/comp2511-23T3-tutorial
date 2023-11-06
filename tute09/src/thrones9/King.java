ae7a4d4bc94468f0752e7b70a6734223540f62c4
Alvin Cherk <a.cherk@unsw.edu.au>
2023-11-06 11:47:54 +1100

    Week 9 code

--- tute09/src/thrones9/King.java
+++ tute09/src/thrones9/King.java
@@ -7,6 +7,20 @@ package thrones9;
  * @author Robert Clifton-Everest
  *
  */
-public class King {
+public class King extends Character {
+
+    public King(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void attack(Character victim) {
+        victim.damage(8);
+    }
+
+    @Override
+    public boolean canMove(int dx, int dy) {
+        return dx <= 1 && dy <= 1;
+    }
 
 }
--- tute09/src/thrones9/Queen.java
+++ tute09/src/thrones9/Queen.java
@@ -1,5 +1,7 @@
 package thrones9;
 
+import java.util.Random;
+
 /**
  * A queen can move to any square in the same column, row or diagonal as she is
  * currently on, and has a 1 in 3 chance of inflicting 12 points of damage or a
@@ -8,6 +10,37 @@ package thrones9;
  * @author Robert Clifton-Everest
  *
  */
-public class Queen {
+public class Queen extends Character {
+    public Queen(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void attack(Character victim) {
+        Random r = new Random(1323);
+        if (r.nextInt(3) == 2) {
+            // [0,3) => 0, 1, 2
+            // 1/3
+            victim.damage(12);
+        } else {
+            // 2/3
+            victim.damage(6);
+        }
+    }
+
+    @Override
+    public boolean canMove(int dx, int dy) {
+        // 1. Diagonal
+        if (dx == dy) {
+            return true;
+        } else if (dx == 0 && dy != 0) {
+            // 2. vertical
+            return true;
+        } else if (dy == 0 && dx != 0) {
+            // 3. horizontal
+            return true;
+        }
+        return false;
+    }
 
 }
